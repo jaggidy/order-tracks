742216975113fc77f3002d72a58a82da8ed82297
jaggidy <shurellaz@gmail.com>
2025-07-20 14:59:09 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,15 @@
+import { SessionProvider } from "next-auth/react";
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head>
         <title>Order Tracker</title>
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Wrap your entire app in SessionProvider */}
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
